4fc8b38a7fb5d2044d093cae65e0deaa341a38e1
Clown Monster <mk6386223@gmail.com>
2020-02-13 20:05:54 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+
+lang = ('Java', 'python', 'C++','C','html','css')
+profficiency = [9,9.3,4,8,6.7,5]
+
+y = np.arange(len(lang))
+
+
+plt.xlabel('Languages')
+plt.ylabel('profficiency')
+plt.title('Test Graph')
+plt.bar(y, profficiency, align='center', alpha = 0.9)
+plt.xticks(y, lang)
+plt.show()
