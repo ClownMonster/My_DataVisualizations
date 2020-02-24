b4cbf1bf90c04d3f9c04b5766097a6a2a792efb8
Clown Monster <mk6386223@gmail.com>
2020-02-24 21:01:08 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,13 +1,9 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-
 lang = ('Java', 'python', 'C++','C','html','css')
 profficiency = [9,9.3,4,8,6.7,5]
-
 y = np.arange(len(lang))
-
-
 plt.xlabel('Languages')
 plt.ylabel('profficiency')
 plt.title('Test Graph')
